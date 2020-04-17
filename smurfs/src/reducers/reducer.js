0f2459b5f19f898ddf321f26cd104491be9aeed8
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-17 13:05:09 -0400

    reducer setup

--- smurfs/src/reducers/reducer.js
+++ smurfs/src/reducers/reducer.js
@@ -1,1 +1,23 @@
-//imports
+import {FETCH_SMURFS_START, FETCH_SMURFS_SUCCESS, FETCH_SMURFS_FAILURE} from '../actions/Actions';
+
+
+const initialState = {
+
+smurfs: [
+  ],
+isFetching: false,
+error: '',
+};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_SMURFS_START:
+        return {smurfs:[], isFetching: true, error: '' }
+        case FETCH_SMURFS_SUCCESS:
+        return {smurfs: action.payload, isFetching: false, error: '' }
+        case FETCH_SMURFS_FAILURE:
+        return {smurfs: [], isFetching: false, error: action.payload }
+        default: 
+        return state
+    }
+}
