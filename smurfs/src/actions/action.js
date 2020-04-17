5c8d0f0d20c7dd8d299c67997d6f3e30d994efc5
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-17 12:29:31 -0400

    fetch exports setup in action.js

--- smurfs/src/actions/action.js
+++ smurfs/src/actions/action.js
@@ -1,1 +1,6 @@
 import axios from 'axios';
+
+
+export const FETCH_SMURF_START = 'FETCH_SMURF_START'
+export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS'
+export const FETCH_SMURF_FAILURE = 'FETCH_SMURF_FAILURE'
