90edaa760d5e39905dca67b1d78e63656f955cfa
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-17 12:42:18 -0400

    axios setup in action

--- smurfs/src/actions/action.js
+++ smurfs/src/actions/action.js
@@ -4,3 +4,18 @@ import axios from 'axios';
 export const FETCH_SMURF_START = 'FETCH_SMURF_START'
 export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS'
 export const FETCH_SMURF_FAILURE = 'FETCH_SMURF_FAILURE'
+
+export const fetchSmurfs = () => {
+    return dispatch => {
+        dispatch({type: FETCH_SMURFS_START})
+    axios
+    .get('https://ghibliapi.herokuapp.com/films')
+    .then(res => {
+        dispatch({type: FETCH_SMURFS_SUCCESS, payload: res.data})
+
+    })
+    .catch(err => {
+        dispatch({type: FETCH_SMURFS_FAILURE, payload: err.res.statusText})
+    })
+    }
+}
