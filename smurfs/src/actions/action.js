fdc71ce0f8b75ab32c805e253cb5dfdde52b2c06
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-17 13:27:59 -0400

    axios post setup for new smurf

--- smurfs/src/actions/action.js
+++ smurfs/src/actions/action.js
@@ -23,3 +23,18 @@ export const fetchSmurfs = () => {
     })
     }
 }
+
+export const addSmurfs = (addSmurf) => {
+    return dispatch => {
+        dispatch({type: POST_SMURFS_START})
+    axios
+    .post('http://localhost:3333/smurfs', addSmurf)
+    .then(res => {
+        dispatch({type: POST_SMURFS_SUCCESS, payload: res.data})
+
+    })
+    .catch(err => {
+        dispatch({type: POST_SMURFS_FAILURE, payload: err.res.statusText})
+    })
+    }
+}
