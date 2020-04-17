726b7a1f6e13e13ee2140f45d9aa9a3fb4420654
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-17 12:44:21 -0400

    http://localhost:3333/smurfs added to .get

--- smurfs/src/actions/action.js
+++ smurfs/src/actions/action.js
@@ -9,7 +9,7 @@ export const fetchSmurfs = () => {
     return dispatch => {
         dispatch({type: FETCH_SMURFS_START})
     axios
-    .get('https://ghibliapi.herokuapp.com/films')
+    .get('http://localhost:3333/smurfs')
     .then(res => {
         dispatch({type: FETCH_SMURFS_SUCCESS, payload: res.data})
 
