9ffce84b950ee641d1b57265011b7beb6578bf71
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-17 13:22:41 -0400

    post exports setup in action.js

--- smurfs/src/actions/action.js
+++ smurfs/src/actions/action.js
@@ -5,6 +5,10 @@ export const FETCH_SMURFS_START = 'FETCH_SMURF_START'
 export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURF_SUCCESS'
 export const FETCH_SMURFS_FAILURE = 'FETCH_SMURF_FAILURE'
 
+export const POST_SMURFS_START = 'FETCH_SMURF_START'
+export const POST_SMURFS_SUCCESS = 'FETCH_SMURF_SUCCESS'
+export const POST_SMURFS_FAILURE = 'FETCH_SMURF_FAILURE'
+
 export const fetchSmurfs = () => {
     return dispatch => {
         dispatch({type: FETCH_SMURFS_START})
